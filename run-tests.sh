289ee155ab5850a4def33b1c8d1655e204ab714c
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2019-11-06 10:07:34 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,11 +5,8 @@ curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo 'Nodejs installation done'
 
-echo $SP_WEB_USERNAME
-echo $SP_WEB_PASSWORD
-
 # move to test code
-cd testpuppeteer
+cd $REPO_FOLDER
 mkdir -p output
 
 #install npm og andre avhenginger
