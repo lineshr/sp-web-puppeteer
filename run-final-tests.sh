ad67574801e15ca7f4c1a289e3391b1e57be8f03
LRaghawan <linesh.raghawan@digdir.no>
2020-09-03 14:01:28 +0200

    Update run-final-tests.sh

--- run-final-tests.sh
+++ run-final-tests.sh
@@ -2,18 +2,3 @@
 
 
 echo "first stage"
-
-VERSION=${1:-latest}
-
-echo "second stage"
-echo "Pulling image ${VERSION}"
-mkdir report
-
-docker run --rm \
-    -v "$(pwd)"/report/:/app/report/ \
-    lineshr/sp-web-puppeteer-test:${VERSION}
-
-status=$?
-
-echo "Final status ${status}"
-exit ${status}
