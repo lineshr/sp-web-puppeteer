41eb4e07ff916793c1f381979d751b2f31517c55
LRaghawan <linesh.raghawan@digdir.no>
2020-09-14 13:28:24 +0200

    Update and rename run-final-tests.sh to run-tests.sh

---
+++ run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install NodeJS
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo 'Nodejs installation done'
+
+# move to test code
+cd $REPO_FOLDER
+mkdir -p output
+
+#install npm og andre avhenginger
+npm install
+npm run build
+npm install codeceptjs puppeteer mocha-multi randomstring --save-dev
+npx codeceptjs run  --reporter mocha-multi
